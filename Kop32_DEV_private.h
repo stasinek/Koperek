cb28be9561a9dfad2f409cd42566e91e4f35efae
stasinek <sstsoft@wp.pl>
2023-10-22 16:07:03 +0200

    library path  fixed, still relative, bad but at least works

--- Kop32_DEV_private.h
+++ Kop32_DEV_private.h
@@ -5,19 +5,19 @@
 #define KOP32_DEV_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.8.0.2579"
+#define VER_STRING	"0.8.0.2593"
 #define VER_MAJOR	0
 #define VER_MINOR	8
 #define VER_RELEASE	0
-#define VER_BUILD	2579
+#define VER_BUILD	2593
 #define COMPANY_NAME	"SSTSOFT.pl"
-#define FILE_VERSION	"0.8.0.2579"
+#define FILE_VERSION	"0.8.0.2593"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	"Koperek"
 #define LEGAL_COPYRIGHT	"Stanislaw Stasiak"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"Kop32_DEV"
 #define PRODUCT_NAME	"Koperek"
-#define PRODUCT_VERSION	"0.8.0.2579"
+#define PRODUCT_VERSION	"0.8.0.2593"
 
 #endif /*KOP32_DEV_PRIVATE_H*/
